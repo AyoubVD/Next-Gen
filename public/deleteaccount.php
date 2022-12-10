12c185b00330fb94e6f136e2b4bb0d7209c2bacb
JasonVer <74768820+JasonVer@users.noreply.github.com>
2022-12-10 15:54:55 +0100

    delete button

--- public/deleteaccount.php
+++ public/deleteaccount.php
@@ -1,43 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php
-    function test(){
-        $servername = "127.0.0.1:3306";
-        $username = "root";
-        $password = "";
-        $dbname = "iamsocial";
-        
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
-        
-        // sql to delete a record
-        $sql = "DELETE FROM users WHERE id=1";
-        
-        if ($conn->query($sql) === TRUE) {
-          echo "Record deleted successfully";
-        } else {
-          echo "Error deleting record: " . $conn->error;
-        }
-        
-        $conn->close();
-    }
-    ?>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./Css/delete.css">
-    <title>Delete account confirmation</title>
-</head>
-<body>
-    <p>Click the delete button to confirm you want to delete your account.</p>
-    <button onclick="GoBack()" class = "btn btn-cancel">Cancel</button>
-    <button id="Bye" onclick="Confirmed()" class = "btn btn-delete">Delete</button>
+<?php
+$servername= "localhost";
+$username="root";
+$password="";
+$dbname= "iamsocial";
+$teller=0;
 
-    <script src="Scripts/CompleteDelete.js"></script>
-</body>
-</html>
+$connect1= new mysqli($servername,$username,$password,$dbname);
+?>
+<?php
+ 
+ if ($connect1->connect_error){
+     die("connection failed: ". $connect1->connect_error);
+ }
+ else{
+     $sql="SELECT * FROM users";
+ }
+if( isset($_GET['id'])){
+    $id=$_GET['id'];
+}
+ $sql="DELETE FROM `users` WHERE `id` = '$id' ";
+ $qresult= $connect1 -> query($sql);
+ header("Location: ../index.php");
+?>
