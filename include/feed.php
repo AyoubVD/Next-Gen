e51f794f92467a5de5944e040a7ea0fc19a64751
Frederic <fredericduwe1997@gmail.com>
2022-12-15 14:54:47 +0100

    likes count

--- include/feed.php
+++ include/feed.php
@@ -3,19 +3,19 @@ include_once '../GLOBALS.php';
 include_once INCLUDE_PATH . "database.php";
 
 function Getfeed(){
-    $pw_check = $GLOBALS["database"]->query(("SELECT fd.*, u.username, u.profilepic, u.id FROM post as fd JOIN users as u ON (fd.userid) = u.id WHERE u.isDeleted=FALSE AND fd.isDeleted=FALSE ORDER BY fd.postid DESC;"));
+    $pw_check = $GLOBALS["database"]->query(("SELECT fd.*, u.username, u.profilepic, u.id, (SELECT COUNT(*) FROM likes as l WHERE l.postid=fd.postid) AS `like` FROM post as fd JOIN users as u ON (fd.userid) = u.id WHERE u.isDeleted=FALSE AND fd.isDeleted=FALSE ORDER BY fd.postid DESC;"));
     $result = $pw_check->fetch_all(MYSQLI_ASSOC);
     return $result;
 }
 
 function Getfollowfeed($userid){
-    $pw_check = $GLOBALS["database"]->query(("SELECT fd.*, u.username, u.profilepic, u.id FROM post as fd JOIN users as u ON (fd.userid) = u.id JOIN followfeed as ffd ON (u.id)=ffd.userFollowingid WHERE u.isDeleted=FALSE AND fd.isDeleted=FALSE AND ffd.userid = ".$userid." ORDER BY fd.postid DESC;"));
+    $pw_check = $GLOBALS["database"]->query(("SELECT fd.*, u.username, u.profilepic, u.id, (SELECT COUNT(*) FROM likes as l WHERE l.postid=fd.postid) AS `like` FROM post as fd JOIN users as u ON (fd.userid) = u.id JOIN followfeed as ffd ON (u.id)=ffd.userFollowingid WHERE u.isDeleted=FALSE AND fd.isDeleted=FALSE AND ffd.userid = ".$userid." ORDER BY fd.postid DESC;"));
     $result = $pw_check->fetch_all(MYSQLI_ASSOC);
     return $result;
 }
 
 function GetUserfeed($userid){
-    $pw_check = $GLOBALS["database"]->query(("SELECT fd.*, u.username, u.profilepic, u.id FROM post as fd JOIN users as u ON (fd.userid) = u.id WHERE u.isDeleted=FALSE AND fd.isDeleted=FALSE AND userid=".$userid." ORDER BY fd.postid DESC;"));
+    $pw_check = $GLOBALS["database"]->query(("SELECT fd.*, u.username, u.profilepic, u.id, (SELECT COUNT(*) FROM likes as l WHERE l.postid=fd.postid) AS `like` FROM post as fd JOIN users as u ON (fd.userid) = u.id WHERE u.isDeleted=FALSE AND fd.isDeleted=FALSE AND userid=".$userid." ORDER BY fd.postid DESC;"));
     $result = $pw_check->fetch_all(MYSQLI_ASSOC);
     return $result;
 }
