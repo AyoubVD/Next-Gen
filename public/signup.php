315f5d8fa0bd3db114e6bbaf7d2fbf81274079f3
Frederic <fredericduwe1997@gmail.com>
2022-12-15 23:40:23 +0100

    Full sanitize done

--- public/signup.php
+++ public/signup.php
@@ -2,14 +2,16 @@
 include_once '../include/users.php';
 
 if($_POST){
-    $email=$_POST["email"];
-    $username=$_POST["username"];
-    $password=$_POST["password"];
-    $password2=$_POST["password2"];
+    $email=htmlspecialchars($_POST["email"]);
+    $username=htmlspecialchars($_POST["username"]);
+    $password=htmlspecialchars($_POST["password"]);
+    $password2=htmlspecialchars($_POST["password2"]);
     $agree=$_POST["agree"];
-    if(register($username,$password,$password2,$email,$agree)){
-        header('Location: ./index.php');
-    }
+	if ($agree){
+		if(register($username,$password,$password2,$email,$agree)){
+			header('Location: ./index.php');
+		}
+	}
 }
 ?>
 <!doctype html>
@@ -41,29 +43,29 @@ if($_POST){
 						<form action="./signup.php" class="signup-form" method="post" id="demo-form">
 		      		<div class="form-group mb-3">
 		      			<label class="label" for="username">Username</label>
-		      			<input type="text" class="form-control" placeholder="Friendly Neighborhood Spider-Man" name="username" id="username" required>
+		      			<input required type="text" class="form-control" placeholder="Friendly Neighborhood Spider-Man" name="username" id="username" required>
 		      			<span class="icon fa fa-user-o"></span>
 		      		</div>
 		      		<div class="form-group mb-3">
 		      			<label class="label" for="email">Email Address</label>
-		      			<input type="text" class="form-control" placeholder="peterparker@spidey.com" name="email" id="email" required>
+		      			<input required type="text" class="form-control" placeholder="peterparker@spidey.com" name="email" id="email" required>
 		      			<span class="icon fa fa-paper-plane-o"></span>
 		      		</div>
 	            <div class="form-group mb-3">
 	            	<label class="label" for="password">Password</label>
-	              <input id="password" type="password" class="form-control" placeholder="P@$$w0rd" name="password" id="password" required>
+	              <input required id="password" type="password" class="form-control" placeholder="P@$$w0rd" name="password" id="password" required>
 	              <span toggle="#password" class="fa fa-fw fa-eye field-icon toggle-password"></span>
 	              <span class="icon fa fa-lock"></span>
 	            </div>
 	            <div class="form-group mb-3">
 	            	<label class="label" for="password">Password</label>
-	              <input id="password-confirm" type="password" class="form-control" placeholder="P@$$w0rd" name="password2" id="password2" required>
+	              <input required id="password-confirm" type="password" class="form-control" placeholder="P@$$w0rd" name="password2" id="password2" required>
 	              <span toggle="#password-confirm" class="fa fa-fw fa-eye field-icon toggle-password"></span>
 	              <span class="icon fa fa-lock"></span>
 	            </div>
                 <div>
              <label for="agree">
-                <input type="checkbox" name="agree" id="agree" value="yes"/> I agree
+                <input required type="checkbox" name="agree" id="agree" value="yes"/> I agree
                 with the
                 <a href="#" title="term of services">term of services</a>
                 
